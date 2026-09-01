8b52492a2bb40b5828a7a818b80c17664b1c258c
Ajeet Chaurasiya <cvajeetchaurasiya8887@gmail.com>
2026-09-01 14:57:44 +0530

    ...

---
+++ App.jsx
@@ -0,0 +1,16 @@
+
+
+import Book from "./book"
+function App(){
+    return
+        <div>
+            <h1>
+                <center>MyBook Store</center>
+            </h1>
+            <div>
+                <Book/>
+            </div>
+        </div>
+
+}
+export default App;
---
+++ book.jsx
@@ -0,0 +1,9 @@
+function Book(){
+    return <div>
+        <img src="" width={100} height={100} alt="Book image"/>
+        <h2>Title:ReactJS</h2>
+        <h2>Price:₹465/-</h2>
+        <button>AddToCart</button>
+    </div>
+}
+export default Book;
